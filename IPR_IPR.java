79abbda832fefc0349e74c524f97b02f11477295
BHARGAV <55741416+Bhargavr216@users.noreply.github.com>
2026-02-16 14:10:53 +0530

    Update IPR_IPR.java

--- IPR_IPR.java
+++ IPR_IPR.java
@@ -1,60 +1,90 @@
-package au.com.coles.otf.utilities;
+[
+    [
+    {
+      "id": "dcc-test-01",
+      "source": "EOS",
+      "type": "eos.orders.allocated",
+      "data": {
+        "orderId": "dcc-1-1001",
+        "status": "Allocated"
+      },
+      "time": "2025-10-30T10:28:21.7223026+00:00",
+      "specversion": "1.0",
+      "dataschema": "#",
+      "datacontenttype": "application/json",
+      "xrequestid": "6f3483b16b13d94d8df7f7443f1d717a",
+      "traceparent": "00-6f3483b16b13d94d8df7f7443f1d717a-5f3771e6a0385f95-00"
+    }
+  ],
+  [
+    {
+      "id": "dcc-test-02",
+      "source": "EOS",
+      "type": "eos.orders.allocated",
+      "data": {
+        "orderId": "col-dcc1007",
+        "status": "Allocated"
+      },
+      "time": "2025-10-30T10:28:21.7223026+00:00",
+      "specversion": "1.0",
+      "dataschema": "#",
+      "datacontenttype": "application/json",
+      "xrequestid": "6f3483b16b13d94d8df7f7443f1d717a",
+      "traceparent": "00-6f3483b16b13d94d8df7f7443f1d717a-5f3771e6a0385f95-00"
+    }
+  ]
+]
 
-import java.util.Map;
 
-import org.apache.log4j.Logger;
 
-import au.com.coles.otf.constant.DCCConstants;
-import au.com.coles.otf.stepdefinitions.DCC;
+[
+ {
+  "event_id": "dcc-test-01",
+  "event_type": "eos.orders.allocated",
+  "event_datetime": "2025-10-30 10:28:21.722303+00",
+  "event_payload": "[{\"id\":\"dcc-test-01\",\"source\":\"EOS\",\"type\":\"eos.orders.allocated\",\"data\":{\"orderId\":\"dcc-1-1001\",\"status\":\"Allocated\"},\"time\":\"2025-10-30T10:28:21.7223026+00:00\",\"specversion\":\"1.0\",\"dataschema\":\"#\",\"datacontenttype\":\"application/json\",\"xrequestid\":\"6f3483b16b13d94d8df7f7443f1d717a\",\"traceparent\":\"00-6f3483b16b13d94d8df7f7443f1d717a-5f3771e6a0385f95-00\"}]",
+  "poi_event_payload": "[{\"specversion\":\"1.0\",\"type\":\"dss.orders.liq.exptype\",\"source\":\"DCC\",\"id\":\"a8cb4ed6-fabe-47e6-8acd-06195a51d662\",\"time\":\"2026-02-16T16:26:25.591662847+11:00\",\"dataschema\":\"#\",\"datacontenttype\":\"application/json\",\"data\":{\"orderId\":\"dcc-1-1001\",\"complianceRequired\":\"ID25\",\"sameDay\":false,\"vicFirstTimeLiq\":null,\"updatedDateTime\":\"2026-02-13T12:37:19Z\",\"associationId\":\"20260213123719\",\"liq\":true}}]",
+  "event_operation": "PUBLISH_POI_DATA",
+  "retry_attempts": "0",
+  "job_set_time": "NULL"
+ }
+]
 
-import com.azure.core.amqp.AmqpTransportType;
-import com.azure.core.credential.TokenCredential;
-import com.azure.identity.ClientSecretCredential;
-import com.azure.identity.ClientSecretCredentialBuilder;
-import com.azure.identity.DefaultAzureCredentialBuilder;
-import com.azure.identity.ManagedIdentityCredentialBuilder;
-import com.azure.messaging.eventhubs.EventData;
-import com.azure.messaging.eventhubs.EventDataBatch;
-import com.azure.messaging.eventhubs.EventHubClientBuilder;
-import com.azure.messaging.eventhubs.EventHubProducerClient;
 
-public class EventTriggerUtil {
 
-	EventTriggerUtil() {
 
-	}
-	private static Logger logs = Logger.getLogger(KeyvaultConnectorApplication.class);
-	public static boolean eventTrigger(String testDataPath) throws Exception {
 
+[
+ {
+  "audit_id": "3733",
+  "event_id": "event-dcc-01",
+  "event_operation": "ORD_PROC_EVENT",
+  "activity_start_datetime": "2026-02-10 17:45:03.5566",
+  "activity_end_datetime": "2026-02-10 17:45:03.620409",
+  "audit_datetime": "2026-02-10 17:45:03.620588",
+  "exception": "NULL"
+ },
+ {
+  "audit_id": "3734",
+  "event_id": "event-dcc-01",
+  "event_operation": "PUBLISH_POI_DATA",
+  "activity_start_datetime": "2026-02-10 17:45:04.117116",
+  "activity_end_datetime": "2026-02-10 17:45:05.758123",
+  "audit_datetime": "2026-02-10 17:45:05.771213",
+  "exception": "NULL"
+ }
+]
+
+
+
+@Success_Scenarios_Create
+Feature: [TECH]: DCC - US1 DCC
+  @DCC
+ 
+		@Phase2
+  	Scenario Outline: Validate DB data for event payload
+  	Given payload file "src/main/resources/payloads/event_payload.json" and expected file "src/main/resources/expected/dcc_expected_data.json" and schema dir "src/main/resources/schemas"
+    Then database values should match expected data
 		
-		Map<String, String> keyvault = KeyvaultConnectorApplication.getKeyVaultDetails();
-		
-	
-		
-		String eventHubConnectionString = keyvault.get("eventHubConnectionString");
-		String fullyQualifiedNamespace = "EVH-OTF-MFS-DEV-AUE.servicebus.windows.net";
-		
-		String eventHubName1 = "orderfulfilmenthandler";
-		
-
-		logs.info("value of keyvault is:" +keyvault);
-		
-		
-		EventHubProducerClient pclient = new EventHubClientBuilder().connectionString(eventHubConnectionString,eventHubName1).consumerGroup(DCCConstants.DEFAULT) .transportType(AmqpTransportType.AMQP_WEB_SOCKETS).buildProducerClient();
-
-		logs.info("value of keyvault is:" +keyvault);
-
-		// prepare a batch of events to send to the event hub
-		  
-		EventDataBatch batch = pclient.createBatch();
-		
-		String body = JsonFileReader.convertJsonToStringObject(testDataPath + ".json");
-		EventData eventdetails = new EventData(body);
-
-		batch.tryAdd(eventdetails);
-		pclient.send(batch);
-
-		pclient.close();
-			return true;
-	}	
-}
+	      
+	      
